9c0e2c2da0cdb0cf1f2b409106b44a8274b82939
ElisonMartins <martinselison10@gmail.com>
2026-02-21 22:03:05 -0300

    feat: add product model with stock field

---
+++ backend/prisma/migrations/20260221233335/migration.sql
@@ -0,0 +1,45 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `total` on the `Order` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Decimal(10,2)`.
+  - You are about to alter the column `price` on the `OrderItem` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Decimal(10,2)`.
+  - You are about to drop the column `imageUrl` on the `Product` table. All the data in the column will be lost.
+  - You are about to alter the column `price` on the `Product` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Decimal(10,2)`.
+  - Added the required column `cep` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `city` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `email` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `name` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `neighborhood` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `number` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `state` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `street` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `image` to the `Product` table without a default value. This is not possible if the table is not empty.
+  - Made the column `description` on table `Product` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_productId_fkey";
+
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "cep" TEXT NOT NULL,
+ADD COLUMN     "city" TEXT NOT NULL,
+ADD COLUMN     "complement" TEXT,
+ADD COLUMN     "email" TEXT NOT NULL,
+ADD COLUMN     "name" TEXT NOT NULL,
+ADD COLUMN     "neighborhood" TEXT NOT NULL,
+ADD COLUMN     "number" TEXT NOT NULL,
+ADD COLUMN     "state" TEXT NOT NULL,
+ADD COLUMN     "street" TEXT NOT NULL,
+ALTER COLUMN "total" SET DATA TYPE DECIMAL(10,2);
+
+-- AlterTable
+ALTER TABLE "OrderItem" ALTER COLUMN "price" SET DATA TYPE DECIMAL(10,2);
+
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "imageUrl",
+ADD COLUMN     "image" TEXT NOT NULL,
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "description" SET NOT NULL,
+ALTER COLUMN "price" SET DATA TYPE DECIMAL(10,2),
+ALTER COLUMN "stock" DROP DEFAULT;
+DROP SEQUENCE "Product_id_seq";
